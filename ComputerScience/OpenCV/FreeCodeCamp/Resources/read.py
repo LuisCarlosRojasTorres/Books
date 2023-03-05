7f7ad44aa4164e8c13b0ead89c4d532c9e9ee028
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-03-05 10:46:10 -0300

    FreeCodeCamp OpenCV
    Changelog:
    
    List of Changes:
    - Added How to open a file
    - Added How to open a video
    List of TODOs:

---
+++ ComputerScience/OpenCV/FreeCodeCamp/Resources/read.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+## Open a jpeg file
+'''
+img = cv.imread('Photos/cat_large.jpg')
+cv.imshow("i", img)
+cv.waitKey(0)
+'''  
+
+## Open a video file
+'''
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    cv.imshow('Video', frame)
+    if cv.waitKey(20) & 0xFF== ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+'''
