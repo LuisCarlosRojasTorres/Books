1dd3830d46af79950fde56455fefdbf5d61804a0
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-03-17 10:43:22 -0300

    PyQT CH01 finished
    Changelog:
    - Added folders and files
    - Added readme file
    - Added CH01 file
    - Added CH001a.py file
    
    List of Changes:
    - Added content to CH01
    
    List of TODOs:

---
+++ ComputerScience/Libraries/PyQT/uploads/CH02/ch002a.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtGui import QPixmap
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+    
+    def initializeUI(self):
+        """Set up the application's GUI."""
+        self.setGeometry(100, 100, 250, 250)
+        self.setWindowTitle("QLabel Example")
+        self.setUpMainWindow()
+        self.show()    
+
+    def setUpMainWindow(self):
+        """Create QLabel to be displayed in the main window."""
+        hello_label = QLabel(self)
+        hello_label.setText("Hello")
+        hello_label.move(105, 15)
+        image = "./logo001.png"
+        try:
+            with open(image):
+                world_label = QLabel(self)
+                pixmap = QPixmap(image)
+                world_label.setPixmap(pixmap)
+                world_label.move(25, 40)
+        except FileNotFoundError as error:
+            print(f"Image not found.\nError: {error}")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
